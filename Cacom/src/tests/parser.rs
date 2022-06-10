a5e516e61f64a848ea74cdf3def5fc986f1289a2
Filip Gregor <filip.gregor98@gmail.com>
2022-06-10 13:57:52 +0200

    Add else test

--- Cacom/src/tests/parser.rs
+++ Cacom/src/tests/parser.rs
@@ -10,11 +10,11 @@ fn parse_test() {
     assert!(TopLevelParser::new().parse("1 + (1 + 2)").is_ok());
 
     assert!(TopLevelParser::new().parse("1;2").is_ok());
-    assert!(TopLevelParser::new().parse("1;").is_ok());  
+    assert!(TopLevelParser::new().parse("1;").is_ok());
 
-    assert!(TopLevelParser::new().parse("{}").is_ok());  
-    assert!(TopLevelParser::new().parse("{1}").is_ok());  
-    assert!(TopLevelParser::new().parse("{1;1;1}").is_ok());  
+    assert!(TopLevelParser::new().parse("{}").is_ok());
+    assert!(TopLevelParser::new().parse("{1}").is_ok());
+    assert!(TopLevelParser::new().parse("{1;1;1}").is_ok());
     assert!(TopLevelParser::new().parse("1;{1;1;1};1").is_ok());
 
     assert!(TopLevelParser::new().parse("def foo(a, b) = 1").is_ok());
@@ -24,7 +24,7 @@ fn parse_test() {
     assert!(TopLevelParser::new().parse("1 + {1}").is_ok());
     assert!(TopLevelParser::new().parse("{1;2} + {3;4} * {5}").is_ok());
     assert!(TopLevelParser::new().parse("1 + def foo(a, b)").is_err());
-    TopLevelParser::new().parse("if(1) { 1; }").expect("");
-    assert!(TopLevelParser::new().parse("if (1) { 1; }").is_ok());
+    assert!(TopLevelParser::new().parse("if (1) { 1 }").is_ok());
+    assert!(TopLevelParser::new().parse("if (1) { 1 } else { 2 }").is_ok());
 
 }
