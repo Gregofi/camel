83139ae6a9f28ee2616d31886a566f85ab001a1a
Filip Gregor <filip.gregor98@gmail.com>
2022-07-09 15:06:34 +0200

    Move test into module

--- Cacom/src/tests/parser.rs
+++ Cacom/src/tests/parser.rs
@@ -1,56 +1,66 @@
-use crate::ast::{AST};
-use crate::grammar::TopLevelParser;
+#[cfg(test)]
+mod parser_tests {
+    use crate::grammar::TopLevelParser;
 
-#[test]
-fn expressions_test() {
-    assert!(TopLevelParser::new().parse("1").is_ok());
-    assert!(TopLevelParser::new().parse("1 * 2 * 3").is_ok());
-    assert!(TopLevelParser::new().parse("1 + 2").is_ok());
-    assert!(TopLevelParser::new().parse("1 + 2 + 3").is_ok());
-    assert!(TopLevelParser::new().parse("1 + 2 * 3").is_ok());
-    assert!(TopLevelParser::new().parse("1 + (1 + 2)").is_ok());
-}
+    #[test]
+    fn expressions_test() {
+        assert!(TopLevelParser::new().parse("1").is_ok());
+        assert!(TopLevelParser::new().parse("1 * 2 * 3").is_ok());
+        assert!(TopLevelParser::new().parse("1 + 2").is_ok());
+        assert!(TopLevelParser::new().parse("1 + 2 + 3").is_ok());
+        assert!(TopLevelParser::new().parse("1 + 2 * 3").is_ok());
+        assert!(TopLevelParser::new().parse("1 + (1 + 2)").is_ok());
+    }
+    
+    #[test]
+    fn blocks_test() {
+        assert!(TopLevelParser::new().parse("{}").is_ok());
+        assert!(TopLevelParser::new().parse("{1}").is_ok());
+        assert!(TopLevelParser::new().parse("{1;1;1}").is_ok());
+        assert!(TopLevelParser::new().parse("1;{1;1;1};1").is_ok());
+        assert!(TopLevelParser::new().parse("1;").is_ok());
+        assert!(TopLevelParser::new().parse("1;2").is_ok());
+        assert!(TopLevelParser::new().parse("1 + {1}").is_ok());
+        assert!(TopLevelParser::new().parse("{1;2} + {3;4} * {5}").is_ok());
+    }
+    
+    #[test]
+    fn functions_test() {
+        assert!(TopLevelParser::new().parse("def foo(a, b) = 1").is_ok());
+        assert!(TopLevelParser::new().parse("def foo(a, b) = {1; 3}").is_ok());
+        assert!(TopLevelParser::new().parse("def foo(a, b) = 1; 1").is_ok());
+    
+        // assert!(TopLevelParser::new().parse("return 1").is_err());
+        assert!(TopLevelParser::new().parse("def foo(a, b) = return 1").is_ok());
+        assert!(TopLevelParser::new().parse("1 + def foo(a, b)").is_err());
+    }
+    
+    #[test]
+    fn conditional_test() {
+        assert!(TopLevelParser::new().parse("if (1) { 1 }").is_ok());
+        assert!(TopLevelParser::new().parse("if (1) { 1 } else { 2 }").is_ok());
+        assert!(TopLevelParser::new().parse("if (1 <= 2) { 1 } else { 2 }").is_ok());
+        assert!(TopLevelParser::new().parse("if(1) 1").is_err());
+        assert!(TopLevelParser::new().parse("def foo(a) = if (1 < 2) {1} else {2}").is_ok());
+        assert!(TopLevelParser::new().parse("def foo(a) = if (1 < 2) {1} else {2}; foo(1);").is_ok());
+    }
+    
+    #[test]
+    fn calls_test() {
+        assert!(TopLevelParser::new().parse("foo();").is_ok());
+        assert!(TopLevelParser::new().parse("foo(1, 2);").is_ok());
+        assert!(TopLevelParser::new().parse("print(\"Hi\");").is_ok());
+        assert!(TopLevelParser::new().parse("print(\"{} + {} = {}\", 1, 2, 1 + 2);").is_ok());
+    }
 
-#[test]
-fn blocks_test() {
-    assert!(TopLevelParser::new().parse("{}").is_ok());
-    assert!(TopLevelParser::new().parse("{1}").is_ok());
-    assert!(TopLevelParser::new().parse("{1;1;1}").is_ok());
-    assert!(TopLevelParser::new().parse("1;{1;1;1};1").is_ok());
-    assert!(TopLevelParser::new().parse("1;").is_ok());
-    assert!(TopLevelParser::new().parse("1;2").is_ok());
-    assert!(TopLevelParser::new().parse("1 + {1}").is_ok());
-    assert!(TopLevelParser::new().parse("{1;2} + {3;4} * {5}").is_ok());
-}
-
-#[test]
-fn functions_test() {
-    assert!(TopLevelParser::new().parse("def foo(a, b) = 1").is_ok());
-    assert!(TopLevelParser::new().parse("def foo(a, b) = {1; 3}").is_ok());
-    assert!(TopLevelParser::new().parse("def foo(a, b) = 1; 1").is_ok());
-
-    assert!(TopLevelParser::new().parse("return 1").is_err());
-    assert!(TopLevelParser::new().parse("def foo(a, b) = return 1").is_ok());
-    assert!(TopLevelParser::new().parse("1 + def foo(a, b)").is_err());
-}
-
-#[test]
-fn conditional_test() {
-    assert!(TopLevelParser::new().parse("if (1) { 1 }").is_ok());
-    assert!(TopLevelParser::new().parse("if (1) { 1 } else { 2 }").is_ok());
-    assert!(TopLevelParser::new().parse("if (1 < 2) { 1 } else { 2 }").is_ok());
-    assert!(TopLevelParser::new().parse("if(1) 1").is_err());
-}
-
-#[test]
-fn calls_test() {
-    assert!(TopLevelParser::new().parse("foo();").is_ok());
-    assert!(TopLevelParser::new().parse("foo(1, 2);").is_ok());
-    assert!(TopLevelParser::new().parse("print(\"Hi\");").is_ok());
-    assert!(TopLevelParser::new().parse("print(\"{} + {} = {}\", 1, 2, 1 + 2);").is_ok());
-}
-
-fn string_literals_test() {
-    assert!(TopLevelParser::new().parse("\"Hello, World!\"").is_ok());
-    assert!(TopLevelParser::new().parse("\"Hello\" + \" World!\"").is_ok());
+    #[test]
+    fn identifiers_test() {
+        assert!(TopLevelParser::new().parse("def foo(n) = n + 1").is_ok());
+    }
+    
+    #[test]
+    fn string_literals_test() {
+        assert!(TopLevelParser::new().parse("\"Hello, World!\"").is_ok());
+        assert!(TopLevelParser::new().parse("\"Hello\" + \" World!\"").is_ok());
+    }
 }
