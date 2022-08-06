c968a08a740cf866dcbda8ccf99d6516bc125b29
Filip Gregor <filip.gregor98@gmail.com>
2022-08-06 18:30:56 +0200

    Wrap test macros in do while to force semicolon

--- Caby/tests/test.h
+++ Caby/tests/test.h
@@ -3,19 +3,23 @@
 #include <stdio.h>
 
 #define ASSERT_W(expr)                                                                                      \
+do {                                                                                                        \
 if(!(expr))                                                                                                 \
 {                                                                                                           \
 printf("%s:%d - assertion failed in %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__);                        \
 return EXIT_FAILURE;                                                                                        \
-}
+}                                                                                                           \
+} while (0)
 
 #define ASSERT_EQ(a, b)                                                                                     \
+do {                                                                                                        \
 if((a) != (b))                                                                                              \
 {                                                                                                           \
 printf("%d:%d - assertion failed in %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__);                        \
 printf("a=%d, b=%d\n",a,b);                                                                                 \
 return EXIT_FAILURE;                                                                                        \
-} else void(0)
+} else { void(0); }                                                                                         \
+} while (0)
 
 #define GREEN_COLOR_TERMINAL "\033[0;32m"
 #define RED_COLOR_TERMINAL "\033[0;31m"
@@ -24,9 +28,11 @@ return EXIT_FAILURE;                                                            
 
 #define TEST(name) int name()
 
-#define RUN_TEST(test)                                                                                          \
+#define RUN_TEST(test)                                                                                      \
+do {                                                                                                        \
 if((test)() != 0)                                                                                           \
     printf("Test '%s'" RED_COLOR_TERMINAL " failed.\n" CLEAR_COLOR_TERMINAL, #test);                        \
 else {                                                                                                      \
     printf("Test '%s'" GREEN_COLOR_TERMINAL " completed.\n" CLEAR_COLOR_TERMINAL, #test);                   \
-}
+}}                                                                                                          \
+while (0)
