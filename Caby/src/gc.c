b31a72d811f025fad176ab42ed918934795d2ba0
Filip Gregor <filip.gregor98@gmail.com>
2022-08-25 15:35:07 +0200

    Add marking functions

--- Caby/src/gc.c
+++ Caby/src/gc.c
@@ -1,1 +1,58 @@
 #include "gc.h"
+#include "vm.h"
+#include "object.h"
+#include "hashtable.h"
+
+#define IS_MARKED(val) (val & 1)
+
+static void mark_object(struct object* obj) {
+    if (obj == NULL) {
+        return;
+    }
+    obj->gc_data = 1;
+#ifdef __GC_DEBUG__
+    GC_LOG("Marking object %p: ", obj);
+    dissasemble_object(obj);
+    GC_LOG("\n");
+#endif
+}
+
+static void mark_val(struct value* v) {
+    if (v->type == VAL_OBJECT) {
+        mark_object(v->object);
+    }
+}
+
+static void mark_table(struct table* table) {
+    for (size_t i = 0; i < table->capacity; ++i) {
+        struct entry* e = &table->entries[i];
+        mark_val(&e->key);
+        // TODO
+        // if (e->key.type != VAL_NONE) {
+            mark_val(&e->val);
+        // }
+    }
+}
+
+static void mark_roots(vm_t* vm) {
+    // stack
+    for (size_t i = 0; i < vm->stack_len; ++i) {
+        mark_val(&vm->op_stack[i]);
+    }
+    // globals
+    mark_table(&vm->globals);
+    // locals in frames
+    for (size_t i = 0; i < vm->frame_len; ++i) {
+        for (size_t j = 0; j < vm->frames->function->locals; ++j) {
+            mark_val(&vm->frames[i].slots[j]);
+        }
+    }
+}
+
+void gc_collect(vm_t* vm) {
+    GC_LOG("=== GC BEGIN ===\n");
+    
+    mark_roots(vm);
+
+    GC_LOG("=== GC END ===\n\n");
+}
--- Caby/src/gc.h
+++ Caby/src/gc.h
@@ -1,4 +1,10 @@
 #pragma once
 #include "vm.h"
 
+#ifdef __GC_DEBUG__
+#define GC_LOG(format, ...) do { fprintf(stderr, format, ##__VA_ARGS__); } while (false)
+#else
+#define GC_LOG(format, ...)
+#endif
+
 void gc_collect(vm_t* vm);
