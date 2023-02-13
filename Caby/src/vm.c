2ef23355bcb12b1374dad631834fb1e51bb233cc
Filip Gregor <filip.gregor98@gmail.com>
2023-02-13 21:57:43 +0100

    Pretiffy disassembling

--- Caby/src/vm.c
+++ Caby/src/vm.c
@@ -24,7 +24,7 @@
 static void disassemble_stack(vm_t* vm) {
     for (size_t i = 0; i < vm->stack_len; ++i) {
         fprintf(stderr, "[");
-        disassemble_value(stderr, vm->op_stack[i]);
+        disassemble_value(stderr, vm->op_stack[i], true);
         fprintf(stderr, "]");
     }
     fprintf(stderr, "\n");
